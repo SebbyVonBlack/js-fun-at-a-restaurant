08e347d06bb7f80b3115acfe0a6c8551cfe9cff2
Sebby <sebbyblackdev@gmail.com>
2023-09-20 18:54:31 -0500

    Finished meal work

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,56 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return 'Delicious ' + name;
+}
+
+function createMenuItem() {
+  var createMenuItem =  {
+    name: 'Delicious French Toast',
+    price: 10.99,
+    type: 'breakfast'
+  }
+  return createMenuItem
+}
+
+function addIngredients(ingredient, ingredients) {
+
+
+  for (var i = 0; i < ingredients.length; i++) {
+    if (ingredient === ingredients[i]) {
+      return;
+   
+    }
+  }
+  ingredients.push(ingredient)
+  return ingredients
+}
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`
 
 }
 
+function decreasePrice(price) {
+  let newPrice = price * 0.9
+  return newPrice;
+}
 
+function createRecipe(title, ingredients, menuItemType) {
+  var createRecipe =  {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+  return createRecipe
+}
+
+//test for different price not working, not sure why as it should
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
